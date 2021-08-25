9a50757b9f13178f9e2b00a61c9428e7ce3a32e2
Flávio Araujo <flavio21259@gmail.com>
2021-08-25 13:39:33 -0300

    Finalização do teste End-to-end

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -10,17 +10,15 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     
         });
     
-        it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
-            cy.visit('produtos')
+        it('Deve realizar todos os processos End-to-end - Usando comandos customizados', () => {
+            cy.get('#primary-menu > .menu-item-629 > a').click()
             cy.addProdutos('Aero Daily Fitness Tee', 'M', 'Black', 4)
-
-        });
-    
-        it('Deve realizar o checkout com sucesso - Usando arquivo de dados', 'Deve validar a compra com sucesso', () => {
-            cy.visit('checkout')
+            cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+            cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
             EnderecoPage.editarEnderecoCheckout('Yuki', 'Salazar', 'RC Studio', 'Brasil', 'Rua Solimão', '124', 'Praia Grande', 'São Paulo', '11721060', '11989899988', 'aleksalazar@outlook.com.br')
             cy.get('#terms').click()
             cy.get('#place_order').click()
             cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
         });
+
 });
