39e0f19e8b9bad502beae95515bcbcacf159ba3a
Flávio Araujo <flavio21259@gmail.com>
2021-08-25 13:03:33 -0300

    Qualquer coisa

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,21 +1,26 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page_objects/endereco.page'
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
-        E validando minha compra ao final */
+        beforeEach(() => {
+            cy.visit('minha-conta')
+            cy.fixture('perfil').then(dados => {
+                cy.login(dados.usuario, dados.senha)
+            })
+    
+        });
+    
+        it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+            cy.visit('produtos')
+            cy.addProdutos('Aero Daily Fitness Tee', 'M', 'Black', 4)
 
-    beforeEach(() => {
-        cy.visit('/')
-    });
-
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
-
-
-})
+        });
+    
+        it('Deve realizar o checkout com sucesso - Usando arquivo de dados', 'Deve validar a compra com sucesso', () => {
+            cy.visit('checkout')
+            EnderecoPage.editarEnderecoCheckout('Yuki', 'Salazar', 'RC Studio', 'Brasil', 'Rua Solimão', '124', 'Praia Grande', 'São Paulo', '11721060', '11989899988', 'aleksalazar@outlook.com.br')
+            cy.get('#terms').click()
+            cy.get('#place_order').click()
+            cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+        });
+});
